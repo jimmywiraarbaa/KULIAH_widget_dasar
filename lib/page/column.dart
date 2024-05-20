cc420b7f9a186fde42c609e350add7745919ec95
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-05-20 11:54:46 +0700

    Belajar membuat colom

---
+++ lib/page/column.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Hallo Guys",
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Halo Teman Teman'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            color: Colors.greenAccent,
+            child: const FlutterLogo(
+              size: 80.0,
+            ),
+          ),
+          Container(
+            color: Colors.pinkAccent,
+            child: const FlutterLogo(
+              size: 80.0,
+            ),
+          ),
+          Container(
+            color: Colors.redAccent,
+            child: const FlutterLogo(
+              size: 80.0,
+            ),
+          ),
+        ],
+      ),
+    ),
+  ));
+}
